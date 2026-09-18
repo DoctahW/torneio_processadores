63b2d3926f65b5840b842ba84f8dfe7bb3952f29
Daniel Donaire <danieldc0106@gmail.com>
2026-09-18 19:58:35 -0300

    feat(decode): implementa as operações load, store, e mov no dataflow (#5)

--- src/decode_dataflow.c
+++ src/decode_dataflow.c
@@ -2,14 +2,16 @@
 
 /* LOAD (0x01), STORE (0x02), MOV (0x05). */
 int decode_execute_dataflow(CPU *cpu, uint8_t op, uint8_t a, uint8_t b) {
-    (void)cpu; (void)a; (void)b;
     switch (op) {
-    case 0x01: /* TODO: Donart */
-        break;
-    case 0x02: /* TODO: Donart */
-        break;
-    case 0x05: /* TODO: Donart */
-        break;
+    case 0x01: // load
+        cpu->reg[a] = cpu->mem[b];
+        return 1;
+    case 0x02: // store
+        cpu->mem[b] = cpu->reg[a];
+        return 1;
+    case 0x05: // mov
+        cpu->reg[a] = b;
+        return 1;
     }
     return 0;
 }
