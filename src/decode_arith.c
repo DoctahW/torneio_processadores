16bc4ea4d6597abea791c3f3347d85a2ba9a4b5f
vinici-o <vinicius290371@gmail.com>
2026-09-18 19:20:45 -0300

    feat: ADD, SUB e CMP no decode + flag ZF

--- src/decode_arith.c
+++ src/decode_arith.c
@@ -2,14 +2,18 @@
 
 /* ADD (0x03), SUB (0x04), CMP (0x06) + flag ZF. */
 int decode_execute_arith(CPU *cpu, uint8_t op, uint8_t a, uint8_t b) {
-    (void)cpu; (void)a; (void)b;
     switch (op) {
-    case 0x03: /* TODO: REINICIO */
-        break;
-    case 0x04: /* TODO: REINICIO */
-        break;
-    case 0x06: /* TODO: REINICIO */
-        break;
+    case 0x03:
+        cpu->reg[a] += cpu->reg[b];
+        cpu->zf = (cpu->reg[a] == 0);
+        return 1;
+    case 0x04:
+        cpu->reg[a] -= cpu->reg[b];
+        cpu->zf = (cpu->reg[a] == 0);
+        return 1;
+    case 0x06:
+        cpu->zf = (cpu->reg[a] == cpu->reg[b]);
+        return 1;
     }
     return 0;
 }
