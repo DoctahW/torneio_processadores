ecb2f1164c1856be742925eb3b7d9fbe13ad88d1
ariel-cs <aco4@cesar.school>
2026-09-18 16:00:55 -0300

    feat: implementa instruções de controle (JMP, JZ, JNZ e HALT)

--- src/decode_control.c
+++ src/decode_control.c
@@ -2,16 +2,27 @@
 
 /* JMP (0x07), JZ (0x08), JNZ (0x09), HALT (0x0A). */
 int decode_execute_control(CPU *cpu, uint8_t op, uint8_t a, uint8_t b) {
-    (void)cpu; (void)a; (void)b;
+    (void)b;
     switch (op) {
-    case 0x07: /* TODO: AIREL */
-        break;
-    case 0x08: /* TODO: AIREL */
-        break;
-    case 0x09: /* TODO: AIREL */
-        break;
-    case 0x0A: /* TODO: AIREL */
-        break;
+    case 0x07: /* pula para o endereço a */
+        cpu->pc = a;
+        return 1;
+
+    case 0x08: /* pula para o endereço a se zf == 1 */
+        if(cpu->zf){
+            cpu->pc = a;
+        }
+        return 1;
+
+    case 0x09: /* pula para o endereço a se zf == 0 */
+        if(!cpu->zf){
+            cpu->pc = a;
+        }
+        return 1;
+
+    case 0x0A: /* para  */
+        cpu->running = 0;
+        return 1;
     }
     return 0;
 }
